5cf6256fd084ee4870e2327d7a0071981bffc0a4
kamchai <kamchai2013@gmail.com>
2018-08-03 11:34:42 +0700

    FIX

---
+++ mongodb-detect-db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+๊๊USER=""
+PASSWD=""
+DBNAME=""
+MONGO_DB="$(mongo $DBNAME --quiet --eval "db.getCollectionNames().toString().replace(/,/g, ' ')" --port 27017 -u "$USER" -p "$PASSWD" --host  127.0.0.1  --authenticationDatabase "admin")"
+#echo $MONGO_DB
+
+FIRST_ELEMENT=1
+function check_first_element {
+    if [[ $FIRST_ELEMENT -ne 1 ]]; then
+        printf ","
+    fi
+    FIRST_ELEMENT=0
+}
+printf '{'
+printf ' "data":['
+for TABLES in $MONGO_DB; do
+    #echo $db
+    check_first_element
+        printf "{"
+        printf "\"{#TABLES}\":\"$TABLES\""
+        printf "}"
+done
+printf ' ]'
+printf '}'
---
+++ mongodb-response-time.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DBNAME=""
+TABLES=$1
+OPTION=$2
+USER=""
+PASSWD=""
+
+case $OPTION in
+        total)
+                QUERY="$(mongotop  --rowcount 1 --quiet --json   --port 27017 -u "$USER" -p "$PASSWD" --host  127.0.0.1  --authenticationDatabase "admin" | jq '.totals["'$DBNAME'.'$TABLES'"] .total .time')"
+                ;;
+        read)
+                  QUERY="$(mongotop  --rowcount 1 --quiet --json   --port 27017 -u "$USER" -p "$PASSWD" --host  127.0.0.1  --authenticationDatabase "admin" | jq '.totals["'$DBNAME'.'$TABLES'"] .read .time')"
+                ;;
+        write)
+                 QUERY="$(mongotop  --rowcount 1 --quiet --json   --port 27017 -u "$USER" -p "$PASSWD" --host  127.0.0.1  --authenticationDatabase "admin" | jq '.totals["'$DBNAME'.'$TABLES'"] .write .time')"
+                ;;
+        *)
+            
+             exit 1
+esac
+echo $QUERY
+exit 0
+
